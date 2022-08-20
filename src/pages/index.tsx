c40c9c51a94aa82e2fa83a3184250befe084ea89
Rodrigo <rod_igomail@hotmail.com>
2022-08-20 10:40:42 -0300

    propriedade 'nome' não reconhecida. Questão enviado para a TreinaWeb

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import Apresentacao from "@parciais/index/_apresentacao";
-import Vantagens from "@parciais/index/_vantagens";
-import DuvidasFrequentes from "@parciais/index/_duvidas-frequentes";
 import type { GetStaticProps, NextPage } from "next";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const Produto = yup.object().shape({
+	produto: yup.object().shape({
+		nome: yup.string().min(3, "Digite um nome maior"),
+	}),
+});
 
 export const getStaticProps: GetStaticProps = async () => {
 	return {
@@ -12,11 +18,26 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 const Index: NextPage = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		resolver: yupResolver(Produto),
+	});
+
+	function enviarFormulário(dados: any) {
+		console.log("Dados do formulário: ", dados);
+	}
+
 	return (
 		<div>
-			<Apresentacao />
-			<Vantagens />
-			<DuvidasFrequentes />
+			<form onSubmit={handleSubmit(enviarFormulário)}>
+				<input {...register("produto.nome", { required: true })} />
+				{errors?.produto?.nome?.message}
+				{console.log(errors)}
+				<button>Enviar</button>
+			</form>
 		</div>
 	);
 };
