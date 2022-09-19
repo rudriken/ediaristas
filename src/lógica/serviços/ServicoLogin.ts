c624e03ee3fc2c793080c87c75c31cdf8c128177
Rodrigo <rod_igomail@hotmail.com>
2022-09-19 11:41:43 -0300

    finalizada aula 'Serviço de Login'

---
+++ src/lógica/serviços/ServicoLogin.ts
@@ -0,0 +1,42 @@
+import { LoginFormularioDeDadosInterface } from "lógica/@tipos/FormulárioInterface";
+import { InterfaceDoUsuário } from "lógica/@tipos/InterfaceDoUsuário";
+import { LocalStorage } from "./ServicoArmazenamento";
+import { ServiçoAPI } from "./ServiçoAPI";
+
+export const ServicoLogin = {
+	async entrar(
+		credenciais: LoginFormularioDeDadosInterface
+	): Promise<boolean> {
+		try {
+			const { data } = await ServiçoAPI.post<{
+				acesso: string;
+				refresh: string;
+				token_tipo: string;
+				expira_em: number;
+			}>("/autenticacao/token/", credenciais);
+			LocalStorage.gravar("token", data.acesso);
+			LocalStorage.gravar("token_refresh", data.refresh);
+			ServiçoAPI.defaults.headers.common["Authorization"] =
+				"Bearer " + data.acesso;
+			return true;
+		} catch (erro) {
+			return false;
+		}
+	},
+	sair() {
+		ServiçoAPI.post("/autenticacao/logout", {
+			refresh: LocalStorage.pegar("token_refresh", ""),
+		});
+		LocalStorage.apagar("token");
+		LocalStorage.apagar("token_refresh");
+	},
+	async informacoes(): Promise<InterfaceDoUsuário | undefined> {
+		const token = LocalStorage.pegar("token", "");
+		if (token) {
+			ServiçoAPI.defaults.headers.common.Authorization =
+				"Bearer " + token;
+			return (await ServiçoAPI.get<InterfaceDoUsuário>("/api/eu")).data;
+		}
+		return undefined;
+	},
+};
