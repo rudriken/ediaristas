278e0d7f20aa97b49a7ec4fc98b3a19e6a980c89
Rodrigo <rod_igomail@hotmail.com>
2022-09-20 09:32:16 -0300

    finalizada aula 'Tratamento de Token em Requisições'

--- src/lógica/serviços/ServiçoAPI.ts
+++ src/lógica/serviços/ServiçoAPI.ts
@@ -1,5 +1,6 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { ApiLinksInterface } from "lógica/@tipos/ApiLinksInterface";
+import { LocalStorage } from "./ServicoArmazenamento";
 
 const url = process.env.NEXT_PUBLIC_API;
 console.log(process.env.NEXT_PUBLIC_ABC);
@@ -10,6 +11,56 @@ export const ServiçoAPI = axios.create({
 		"content-type": "application/json",
 	},
 });
+// ( . . . )
+ServiçoAPI.interceptors.response.use(
+	(resposta) => resposta,
+	(erro) => {
+		if (
+			erro.resposta.HTTP === 401 &&
+			erro.resposta.data.código === "token_não_validado"
+		) {
+			lidarComAtualizacaoDoToken(erro);
+		}
+		return Promise.reject(erro);
+	}
+);
+
+async function lidarComAtualizacaoDoToken(erro: {
+	config: AxiosRequestConfig;
+}) {
+	const tokenRefresh = LocalStorage.pegar<string>("token_refresh", "");
+	if (tokenRefresh) {
+		LocalStorage.apagar("token_refresh");
+		LocalStorage.apagar("token");
+		try {
+			const {
+				data,
+			}: {
+				data: {
+					acesso: string;
+					refresh: string;
+					token_tipo: string;
+					expira_em: number;
+				};
+			} = await ServiçoAPI.post("/autenticacao/token/atualizar", {
+				refresh: tokenRefresh,
+			});
+			LocalStorage.gravar("token", data.acesso);
+			LocalStorage.gravar("token_refresh", data.refresh);
+			ServiçoAPI.defaults.headers.common.Authorization =
+				"Bearer " + data.acesso;
+			if (erro.config.headers) {
+				erro.config.headers.Authorization =
+					ServiçoAPI.defaults.headers.common.Authorization;
+			}
+			return ServiçoAPI(erro.config);
+		} catch (err) {
+			return erro;
+		}
+	} else {
+		return erro;
+	}
+}
 
 export function linksResolver(
 	links: ApiLinksInterface[] = [],
