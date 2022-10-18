c6b73fef0ddfb5c8baa642713b4de1d8d520b4b8
Rodrigo <rod_igomail@hotmail.com>
2022-10-18 11:28:59 -0300

    finalizada aula 'Montagem do Formulário Reutilizável para Seleção de Cidades'

--- src/visual/componentes/entradas/FormularioUsuario/Formularios/FormularioCidades.tsx
+++ src/visual/componentes/entradas/FormularioUsuario/Formularios/FormularioCidades.tsx
@@ -1,5 +1,9 @@
+import { Autocomplete, CircularProgress, Typography } from "@mui/material";
+import { CidadeInterface } from "logica/@tipos/EnderecoInterface";
 import useFormularioCidades from "logica/ganchos/componentes/entradas/FormularioUsuario/Formularios/useFormularioCidades";
 import React from "react";
+import CampoDeLasca from "visual/componentes/exibe-dados/CampoDeLasca/CampoDeLasca";
+import CampoDeTexto from "../../CampoDeTexto/CampoDeTexto";
 import { CidadesSelecao } from "../FormularioUsuario.style";
 
 export const FormularioCidades: React.FC<{ estado: string }> = ({ estado }) => {
@@ -10,5 +14,54 @@ export const FormularioCidades: React.FC<{ estado: string }> = ({ estado }) => {
 		aoSelecionarCidade,
 		aoDesselecionarCidade,
 	} = useFormularioCidades(estado);
-	return <CidadesSelecao></CidadesSelecao>;
+	return (
+		<CidadesSelecao>
+			<Autocomplete
+				value={{ cidade: "" } as CidadeInterface}
+				onChange={(_evento, novoValor) => {
+					novoValor && aoSelecionarCidade(novoValor.cidade);
+				}}
+				disablePortal
+				options={cidadesASeremSelecionadas}
+				getOptionLabel={(caracteres) => {
+					return caracteres.cidade;
+				}}
+				loading={listaDeCidades.length === 0}
+				loadingText={"Carregando cidades ..."}
+				style={{ gridArea: "busca-cidade" }}
+				noOptionsText={"Nenhuma cidade com esse nome"}
+				renderInput={({ InputProps, ...outras }) => {
+					return (
+						<CampoDeTexto
+							label={"Busque pelo nome da cidade"}
+							InputProps={{
+								...InputProps,
+								endAdornment: (
+									<>
+										{listaDeCidades.length ? (
+											<i className={"twf-search"} />
+										) : (
+											<CircularProgress
+												size={20}
+												color={"inherit"}
+											/>
+										)}
+										{InputProps.endAdornment}
+									</>
+								),
+							}}
+							required={false}
+							{...outras}
+						/>
+					);
+				}}
+			/>
+			<Typography>Cidades selecionadas</Typography>
+			<CampoDeLasca
+				listaDeItens={cidadesSelecionadas}
+				paraDeletar={aoDesselecionarCidade}
+				mensagemQuandoVazio={"Nenhuma cidade selecionada ainda"}
+			/>
+		</CidadesSelecao>
+	);
 };
