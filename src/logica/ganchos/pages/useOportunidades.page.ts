656eb04ec731e105309ba1b614a60fad9d6abec4
Rodrigo <rod_igomail@hotmail.com>
2022-10-26 12:39:19 -0300

    finalizada aula 'Listagem de Oportunidades - Mobile'

--- src/logica/ganchos/pages/useOportunidades.page.ts
+++ src/logica/ganchos/pages/useOportunidades.page.ts
@@ -1,1 +1,8 @@
-export default function useOportunidades() {}
+import { Oportunidade } from "logica/@tipos/OportunidadeInterface";
+import useMovelAtivo from "../useMovelAtivo";
+
+export default function useOportunidadesTrabalho() {
+	const movel = useMovelAtivo(),
+		oportunidades = [] as Oportunidade[];
+	return { movel, oportunidades };
+}
--- src/pages/oportunidades.tsx
+++ src/pages/oportunidades.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { GetStaticProps } from "next";
-import useOportunidades from "logica/ganchos/pages/useOportunidades.page";
-import { Container } from "@mui/material";
+import useOportunidadesTrabalho from "logica/ganchos/pages/useOportunidades.page";
+import { Container, Typography } from "@mui/material";
 import TituloPagina from "visual/componentes/exibe-dados/TituloPagina/TituloPagina";
+import ListaDeDados from "visual/componentes/exibe-dados/ListaDeDados/ListaDeDados";
 // import {  } from "@estilos/pages/oportunidades.styled";
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -14,11 +15,41 @@ export const getStaticProps: GetStaticProps = async () => {
 };
 
 const Oportunidades: React.FC = () => {
-	useOportunidades();
+	const { movel, oportunidades } = useOportunidadesTrabalho();
 	return (
 		<>
 			<Container sx={{ mb: 5, p: 0 }}>
 				<TituloPagina titulo={"Oportunidades de trabalho"} />
+				{oportunidades.length === 0 ? (
+					movel ? (
+						<>
+							<ListaDeDados
+								cabecalho={
+									<>
+										Data: 01/01/2023
+										<br />
+										Limpeza Pesada
+										<br />
+										R$ 240,00
+									</>
+								}
+								corpo={
+									<>
+										Cidade: Uberlândia
+										<br />
+										Número de cômodos: 2
+									</>
+								}
+							/>
+						</>
+					) : (
+						<>Desktop</>
+					)
+				) : (
+					<Typography align={"center"}>
+						Nenhuma oportunidade ainda
+					</Typography>
+				)}
 			</Container>
 		</>
 	);
