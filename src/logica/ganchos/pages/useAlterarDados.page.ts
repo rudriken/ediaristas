e57ce64adb14b36ab70092efe91f1eef50e001f5
Rodrigo <rod_igomail@hotmail.com>
2022-12-16 10:54:11 -0300

    finalizada aula 'Atualização da Foto'

--- src/logica/ganchos/pages/useAlterarDados.page.ts
+++ src/logica/ganchos/pages/useAlterarDados.page.ts
@@ -1,8 +1,13 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { CadastroDiaristaFormularioDeDadosInterface } from "logica/@tipos/FormularioInterface";
-import { TipoDoUsuario } from "logica/@tipos/InterfaceDoUsuario";
+import {
+	InterfaceDoUsuario,
+	TipoDoUsuario,
+} from "logica/@tipos/InterfaceDoUsuario";
 import { ContextoUsuario } from "logica/contextos/ContextoUsuario";
+import { ServicoAPIHateoas } from "logica/servicos/ServicoAPI";
 import { ServicoEstruturaFormulario } from "logica/servicos/ServicoEstruturaFormulario";
+import { ServicoObjeto } from "logica/servicos/ServicoObjeto";
 import { ChangeEvent, useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
@@ -39,10 +44,35 @@ export default function useAlterarDados() {
 		}
 	}
 
+	async function atualizarFoto() {
+		ServicoAPIHateoas(
+			usuario.links,
+			"alterar_foto_usuario",
+			async (requisicao) => {
+				const listaDeArquivos = formularioMetodos.getValues(
+					"usuario.foto_usuario"
+				);
+				if (listaDeArquivos && listaDeArquivos.length) {
+					const fotoDoUsuario = listaDeArquivos[0];
+					try {
+						const dadosDoUsuario = ServicoObjeto.jsonParaFormData({
+							foto_usuario: fotoDoUsuario,
+						});
+						await requisicao<InterfaceDoUsuario>({
+							data: dadosDoUsuario,
+							headers: { "Content-Type": "multipart/form-data" },
+						});
+					} catch (erro) {}
+				}
+			}
+		);
+	}
+
 	return {
 		usuario,
 		formularioMetodos,
 		foto,
 		aoAlterarFoto,
+		atualizarFoto,
 	};
 }
