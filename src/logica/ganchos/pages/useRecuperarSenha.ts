5394926642c9d8e73832fbc04d3fb2d4ddabfeb9
Rodrigo <rod_igomail@hotmail.com>
2022-12-21 11:40:51 -0300

    finalizada aula 'Requisição para Criar Uma Nova Senha'

--- src/logica/ganchos/pages/useRecuperarSenha.ts
+++ src/logica/ganchos/pages/useRecuperarSenha.ts
@@ -2,9 +2,11 @@ import { ContextoServicosExternos } from "logica/contextos/ContextoServicosExter
 import { ServicoAPIHateoas } from "logica/servicos/ServicoAPI";
 import { useContext, useState } from "react";
 
-export function useRecuperarSenha() {
+export function useRecuperarSenha(token = "") {
 	const { estadoServicosExternos } = useContext(ContextoServicosExternos),
 		[email, alterarEmail] = useState(""),
+		[senha, alterarSenha] = useState(""),
+		[confirmacaoDaSenha, alterarConfirmacaoDaSenha] = useState(""),
 		[mensagemFeedback, alterarMensagemFeedback] = useState("");
 
 	async function pedirTokenRecuperacao() {
@@ -25,10 +27,44 @@ export function useRecuperarSenha() {
 		}
 	}
 
+	async function resetarSenha() {
+		if (email.length < 8) {
+			alterarMensagemFeedback("Digite um E-mail válido!");
+			return;
+		}
+		if (senha.length < 8) {
+			alterarMensagemFeedback("Senha muito curta!");
+			return;
+		}
+		if (senha !== confirmacaoDaSenha) {
+			alterarMensagemFeedback("As senhas estão diferentes!");
+			return;
+		}
+		ServicoAPIHateoas(
+			estadoServicosExternos.servicosExternos,
+			"confirmar_alteracao_senha",
+			async (requisicao) => {
+				try {
+					await requisicao({
+						data: { token: token, email: email, senha: senha },
+					});
+					alterarMensagemFeedback("Senha resetada!");
+				} catch (erro) {
+					alterarMensagemFeedback("Erro ao resetar senha!");
+				}
+			}
+		);
+	}
+
 	return {
 		email,
 		alterarEmail,
+		senha,
+		alterarSenha,
+		confirmacaoDaSenha,
+		alterarConfirmacaoDaSenha,
 		pedirTokenRecuperacao,
+		resetarSenha,
 		mensagemFeedback,
 		alterarMensagemFeedback,
 	};
