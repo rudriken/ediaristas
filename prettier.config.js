b53327ef7b324963af346af89f73f96bfa8954b7
Rodrigo <rod_igomail@hotmail.com>
2023-01-17 13:28:24 -0300

    retornado ao estado anterior do  arquivo '\prettier.config.js'

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	singleQuote: false, // padroniza e edita strings para aspas duplas
-	// semi: true, // padroniza e edita as linhas para que sempre encerrem com ";"
+	semi: true, // padroniza e edita as linhas para que sempre encerrem com ";"
 };
